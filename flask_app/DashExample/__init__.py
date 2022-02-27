5c784106508305a654f61a74267a43a69bda8b94
jmcarcell <jmcarcell@users.noreply.github.com>
2022-02-27 14:41:58 +0100

    Delete flask stuff

--- flask_app/DashExample/__init__.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint
-
-blueprint = Blueprint(
-    'Displays blueprint',
-    __name__,
-    url_prefix='/displays',
-    template_folder='templates',
-    static_folder='static'
-)
--- flask_app/DashExample/routes.py
+++
@@ -1,9 +0,0 @@
-from . import blueprint
-from flask import render_template
-from flask_login import login_required
-from ..display import dash_display
-
-@blueprint.route('/<pathname>')
-# @login_required
-def app3_template(pathname):
-    return render_template('app3.html', dash_url=f'/dash/{pathname}', pathname=pathname)
