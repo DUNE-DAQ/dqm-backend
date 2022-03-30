c77c61313dab7a2d4f6a4a5b962e976bedd77aae
jmcarcell <jmcarcell@users.noreply.github.com>
2022-03-30 13:44:41 +0200

    Add getters

--- dqm/dqm/templates/models.py
+++ dqm/dqm/templates/models.py
@@ -5,7 +5,19 @@ class OverviewTemplate(models.Model):
     description = models.CharField(max_length=100)
     display = models.JSONField()
 
+    def get_edit_url(self):
+        return f'/templates-overview/{self.name}/edit'
+
+    def get_delete_url(self):
+        return f'/templates-overview/{self.name}/delete'
+
 class SystemTemplate(models.Model):
     name = models.CharField(max_length=30)
     description = models.CharField(max_length=100)
     display = models.JSONField()
+
+    def get_edit_url(self):
+        return f'/templates-system/{self.name}/edit'
+
+    def get_delete_url(self):
+        return f'/templates-system/{self.name}/delete'
