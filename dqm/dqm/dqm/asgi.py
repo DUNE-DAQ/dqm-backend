50b71059135acde3d0243848234354f5b3345759
jmcarcell <jmcarcell@users.noreply.github.com>
2022-04-22 13:10:32 +0200

    Use development settings with asgi

--- dqm/dqm/dqm/asgi.py
+++ dqm/dqm/dqm/asgi.py
@@ -26,7 +26,7 @@ import os
 import django
 from channels.routing import get_default_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dqm.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dqm.development_settings")
 
 django.setup()
 
