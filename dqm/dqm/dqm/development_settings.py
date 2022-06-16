cf7c624f380dcc6481537221d5585efe944637ba
Philip Asher Hamilton <pahamilt@np04-srv-023.cern.ch>
2022-06-16 18:49:01 +0200

    Seems not to like having commented line in middle of cache block.

--- dqm/dqm/dqm/development_settings.py
+++ dqm/dqm/dqm/development_settings.py
@@ -180,7 +180,6 @@ CACHES = {
     "default": {
         "BACKEND": "django_redis.cache.RedisCache",
         "LOCATION": "redis://127.0.0.1:6379/1",
-        #"LOCATION": "redis://"+HOST_IP+":6379/1"
         "OPTIONS": {
             "CLIENT_CLASS": "django_redis.client.DefaultClient"
         },
