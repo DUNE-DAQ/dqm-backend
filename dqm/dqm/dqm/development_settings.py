6e4e7b32e693f67bd2fb7c953f7158fc7c239116
Philip Asher Hamilton <pahamilt@np04-srv-023.cern.ch>
2022-06-16 18:41:23 +0200

    Undoing IP change for testing.

--- dqm/dqm/dqm/development_settings.py
+++ dqm/dqm/dqm/development_settings.py
@@ -179,8 +179,8 @@ STATICFILES_DIRS = [
 CACHES = {
     "default": {
         "BACKEND": "django_redis.cache.RedisCache",
-        #"LOCATION": "redis://127.0.0.1:6379/1",
-        "LOCATION": "redis://"+HOST_IP+":6379/1"
+        "LOCATION": "redis://127.0.0.1:6379/1",
+        #"LOCATION": "redis://"+HOST_IP+":6379/1"
         "OPTIONS": {
             "CLIENT_CLASS": "django_redis.client.DefaultClient"
         },
