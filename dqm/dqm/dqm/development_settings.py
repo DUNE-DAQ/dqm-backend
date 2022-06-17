bfce9786040986046979c638785322bafd879e15
Philip Asher Hamilton <pahamilt@np04-srv-023.cern.ch>
2022-06-17 15:49:37 +0200

    Trying a hard-coded IP for redis to see if it works.

--- dqm/dqm/dqm/development_settings.py
+++ dqm/dqm/dqm/development_settings.py
@@ -179,7 +179,7 @@ STATICFILES_DIRS = [
 CACHES = {
     "default": {
         "BACKEND": "django_redis.cache.RedisCache",
-        "LOCATION": "redis://127.0.0.1:6379/1",
+        "LOCATION": "redis://10.73.136.36:6379/1",
         "OPTIONS": {
             "CLIENT_CLASS": "django_redis.client.DefaultClient"
         },
