26741ce7260c7c2cfc14b1d026c650553784c4b8
Philip Asher Hamilton <pahamilt@np04-srv-018.cern.ch>
2022-08-25 15:32:34 +0200

    Adding parameterisation of redis host for CHANNEL_LAYERS

--- dqm/dqm/dqm/development_settings.py
+++ dqm/dqm/dqm/development_settings.py
@@ -193,7 +193,7 @@ CHANNEL_LAYERS = {
     'default': {
         'BACKEND': 'channels_redis.core.RedisChannelLayer',
         'CONFIG': {
-            'hosts': [('127.0.0.1', 6379),],
+            'hosts': [(REDIS_HOST, 6379),],
         },
     },
 }
