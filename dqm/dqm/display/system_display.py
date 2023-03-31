0d0f8a208d6e7783154c88b5f02ee1ae07277376
Philip Asher Hamilton <pahamilt@np04-srv-018.cern.ch>
2023-03-31 14:30:45 +0200

    Fixing stupid python error

--- dqm/dqm/display/system_display.py
+++ dqm/dqm/display/system_display.py
@@ -168,7 +168,7 @@ def create_display(overview_name, name):
                     [State(f'{pathname}-graph-{i}', 'relayoutData')]
                 )
                 def plot_line(data=None, reference_run=None, rewind_run=None, relayout_data=None, source=source, stream=key):
-                    if data=None:
+                    if data is None:
                         data={}
                     #dic, date = data
                     print('Data:')
